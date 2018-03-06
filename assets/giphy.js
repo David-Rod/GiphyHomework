93e8940dd6e9c85f88ccd5fa72c0009be41ffe33
Davrod29 <davidrod805@gmail.com>
2018-03-06 11:21:23 -0700

    added functions and api get calls in js file

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,31 +1,74 @@
-//animals will be my topic array
-// i set it to animals to begin with 
-var animals = ["dog", "cat", "bird", "whale"];
-//store api key directly in the script
-var authKey = "2q0DIq0cyJZaFDnyUWGdmTKS6U4QtRRH";
+var animals = ["dog", "cat", "bird", "whale", "lion", "elephant", "shark"];
+var authKey = "jcXPqO7MwHscMkWuKM58GTSrAXRL30Mw";
+
+//variables specific for the animal buttons from array or search input
+
+var q = $("#animal-input").text;
 
 
-var searchTerm = q;
-var q = "";
-//limit variable set to 10 as specified by instructions
-var limti: 10;
-//add api key to the url here
-var queryURLBase = " https://giphy.com.json?api-key=2q0DIq0cyJZaFDnyUWGdmTKS6U4QtRRH" +
-    authKey + "&q=";
+var makeButtons = function () {
+    for (var i = 0; i < animals.length; i++) {
+        //create buttton with text by looping through array
+        var animal = $("<button id='animalbuttons'>");
+        animal.text(animals[i]);
+        $("#animalbutton").append(button);
+}
+}
+makeButtons();
 
-// FUNCTIONS
-function runQuery(queryURL) {
+
+$("#animalbuttons").on("click", function () {
+    var queryURLBase = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=" + authKey + "&limit=10";
+
+    $.ajax({
+        url: queryURLBase,
+        method: "GET"
+    }).done(function (response) {
+
+        console.log(response);
+
+        // for (var i = 0; i < animals.length; i++) {
+        //     //create buttton with text by looping through array
+        //     var animal = $("<button id='animalbuttons'>");
+        //     animal.text(animals[i]);
+        //     $("#animalbutton").append(button);
+
+            //create gif image and append to the div that contains gif images
+            var animalImg = $("<img>");
+            animalImg.attr("src", response.data[i].image.fixed_height.url);
+            $("#animalgifs").append(animalImg);
+        });
+
+
+    }
+
+);
+
+$("#add-animal").on("click", function () {
+    var queryURLBase = "https://api.giphy.com/v1/gifs/search?q=" +
+        q + "&api_key=" + authKey + "&limit=10";
 
 
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function (animalInfo) {
+        url: queryURLBase,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response);
 
+        //loop through to see if the search item matches any items in animals array
+        for (var j = 0; j < animals.length; j++) {
+            if (q !== animals[j]) {
+                //add search item to array if it does not already exist
+                animals.push(q);
+                //look to see if length has been updated
+                console.log(animals.length);
+            }
+            var animalImg = $("<img>");
+            animalImg.attr("src", response.data[i].images.fixed_height.url);
         }
-
-
-
+    });
+});
 
 // there should be a search that appends our array when item is searched
 //q is the search string for an api
